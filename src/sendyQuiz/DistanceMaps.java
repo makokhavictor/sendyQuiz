c3ddf7263fb36b3ce26ce6d3211821dd3b7bfb66
Makokha Victor <marcosvictos@gmail.com>
2016-07-14 15:27:04 +0300

    Added distanceMaps class

--- src/sendyQuiz/DistanceMaps.java
+++ src/sendyQuiz/DistanceMaps.java
@@ -7,9 +7,22 @@ import javax.ws.rs.core.MediaType;
 //WebTarget target = client.target("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=Nairobi&destinations=Kisumu&key=AIzaSyAH66vk6sJooQ9au-PmAu5LIrO3X5BNMso");
 //System.out.println(target.request(MediaType.APPLICATION_JSON_TYPE).get(String.class));
 public class DistanceMaps {
-	public DistanceMaps(){
-		System.out.println("Samson");
+	protected String distanceMatrixHostUrl = "https://maps.googleapis.com/maps/api/distancematrix/";
+	protected String units = "metric";
+	protected String origins;
+	protected String destinations;
+	protected String mode = "json";
+	private String APIKEY = "AIzaSyAH66vk6sJooQ9au-PmAu5LIrO3X5BNMso";
+	
+	public DistanceMaps(String[][] ordersDetails){
+
+		for(int j = 0; j< ordersDetails.length; j++){
+			System.out.println(ordersDetails[j][2]);
+		}
 	}
+	
+	
+	
 	public static double locationsDistanceDifference(String _origin, String _location){
 		return 0;
 	}
--- src/sendyQuiz/Main.java
+++ src/sendyQuiz/Main.java
@@ -1,10 +1,20 @@
 package sendyQuiz;
-
+import java.util.HashMap;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DistanceMaps m = new DistanceMaps();
+//		HashMap<String, String> orders = new HashMap<>();
+//		orders.put("Name","Wangume");
+//		orders.put("Origin", "Nairobi");
+//		orders.put("Destination", "Thika");
+//		
+//		System.out.println(orders);
+
+		String[][] orders = {{"Foo","Bar","de"},{"Test","Test34","et"}};
+		DistanceMaps m = new DistanceMaps(orders);
+
+		
 
 	}
 
